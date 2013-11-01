fd149872611388d8ff227b0cf8863b391922d3dd
James Page <james.page@canonical.com>
2013-11-01 21:42:31 +0000

    HAProxy != ceph interface

--- hooks/cinder_contexts.py
+++ hooks/cinder_contexts.py
@@ -46,7 +46,7 @@ class CephContext(OSContextGenerator):
 
 
 class HAProxyContext(OSContextGenerator):
-    interfaces = ['ceph']
+    interfaces = ['cinder-haproxy']
 
     def __call__(self):
         '''
